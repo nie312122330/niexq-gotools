5094a386af8ecbe23278d0af38767594f005da03
nie312122330 <niexiaoqiang@sina.cn>
2020-10-15 13:05:35 +0800

    切片遍历文件夹

--- filedir/FileOp.go
+++ filedir/FileOp.go
@@ -22,3 +22,20 @@ func TraverseDir(dirPth string, fileList *list.List) error {
 	}
 	return nil
 }
+
+//递归文件夹获取到所有文件名称
+func TraverseDirBySlice(dirPth string, fileList []string) error {
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return err
+	}
+	PthSep := string(os.PathSeparator)
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			TraverseDirBySlice(dirPth+PthSep+fi.Name(), fileList)
+		} else {
+			fileList = append(fileList, dirPth+PthSep+fi.Name())
+		}
+	}
+	return nil
+}

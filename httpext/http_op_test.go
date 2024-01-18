bde3ca921d34205ef9d435327ddf59b20d8de6ea
mi <46509964+nie312122330@users.noreply.github.com>
2024-01-18 17:59:42 +0800

    增加测试

--- httpext/http_op_test.go
+++ httpext/http_op_test.go
@@ -4,6 +4,8 @@ import (
 	"net/url"
 	"testing"
 	"time"
+
+	"github.com/nie312122330/niexq-gotools/fileext"
 )
 
 func TestGet(t *testing.T) {
@@ -28,6 +30,19 @@ func TestPostForm(t *testing.T) {
 		"path": {"asasdfds/2022/05/18/82319253CCA446BB9FB44DBE0E22BBF6.txt"},
 	}
 	str, err := PostForm("http://192.168.0.253:10081/pub/fileExist.do", data, time.Second*10)
+	if nil != err {
+		t.Log(err.Error())
+	} else {
+		t.Log(str)
+	}
+}
+
+func TestPostFile(t *testing.T) {
+	fileBytes, err := fileext.ReadFileByte("http_op.go")
+	extData := url.Values{"dir": {"testd"}}
+	url := "http://192.168.0.253:10081/upload/multiFlies.do"
+
+	str, err := PostFile(url, &fileBytes, "multiFiles", "file.txt", extData, time.Second*10)
 	if nil != err {
 		t.Log(err.Error())
 	} else {
